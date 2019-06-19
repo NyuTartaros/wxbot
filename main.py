6f9cbac08fdd4961170d625ae576166ca3499d58
NyuTartaros <niuyunfei123@live.cn>
2019-06-19 13:24:10 +0800

    init commit

--- main.py
+++ main.py
@@ -7,29 +7,30 @@ from wxpy import *
 
 if __name__ == '__main__':
     # init girl friend info
-    gf_dict = json.load(open('specific_man.json', 'r', encoding='utf-8'))
-    gf_name = gf_dict['name']
-    gf_nickname = gf_dict['NickName']
-    gf_sex = FEMALE if gf_dict['Sex'] == 'FEMALE' else MALE
-    gf_city = gf_dict['city']
+    speci_man_dict = json.load(open('true.json', 'r', encoding='utf-8'))
+    speci_man_name = speci_man_dict['name']
+    speci_man_nickname = speci_man_dict['NickName']
+    speci_man_sex = FEMALE if speci_man_dict['Sex'] == 'FEMALE' else MALE
+    speci_man_city = speci_man_dict['city']
 
     # init Bot
     bot = Bot()
-    gf = bot.friends().search(gf_name, NickName=gf_nickname, Sex=gf_sex, city=gf_city)[0]
+    speci_man = bot.friends().search(speci_man_name, NickName=speci_man_nickname,
+                              Sex=speci_man_sex, city=speci_man_city)[0]
     xiaoice = bot.mps().search('小冰', Signature='我是人工智能微软小冰~~')[0]
 
     @bot.register()
     def monitor_all(msg):
         print(msg)
 
-    @bot.register(gf)
-    def reply_gf(msg):
+    @bot.register(speci_man)
+    def reply_speci_man(msg):
         print(msg)
         xiaoice.send(msg.text)
 
     @bot.register(xiaoice)
     def reply_xiaoice(msg):
         print(msg)
-        gf.send(msg.sender.name + ": " + msg.text)
+        speci_man.send(msg.text)
 
     embed()
